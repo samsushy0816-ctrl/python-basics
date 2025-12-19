63e390854b5f1d5818275c15ef9139e41cff4f9d
Samiksha Mahesh Jadhav <samsushy0816@gmail.com>
2025-12-19 17:42:59 +0530

    Add functions practice

---
+++ functions.py
@@ -0,0 +1,13 @@
+# functions.py
+# Practice program for functions
+
+def greet(name):
+    print("Hello,", name)
+
+def add(a, b):
+    return a + b
+
+greet("Samiksha")
+
+result = add(5, 3)
+print("Sum:", result)
