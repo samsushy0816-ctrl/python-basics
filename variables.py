dff856cc7af8c22959a6cde9802fb782e933fdb0
Samiksha Mahesh Jadhav <samsushy0816@gmail.com>
2025-12-19 14:21:35 +0530

    Add variables practice

---
+++ variables.py
@@ -0,0 +1,10 @@
+# variables.py
+# Basic variable usage in Python
+
+name = "Samiksha"
+age = 21
+is_learning_python = True
+
+print("Name:", name)
+print("Age:", age)
+print("Learning Python:", is_learning_python)
