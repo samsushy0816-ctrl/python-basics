187af6741d3a08337d40e4a5dd1ea5d9bacefa9f
Samiksha Mahesh Jadhav <samsushy0816@gmail.com>
2025-12-19 17:42:24 +0530

    Add loops practice

---
+++ loops.py
@@ -0,0 +1,13 @@
+# loops.py
+# Practice program for loops
+
+# Print numbers from 1 to 5
+for i in range(1, 6):
+    print(i)
+
+print("-----")
+
+# Print even numbers from 1 to 10
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
